c9883faeb2a8078b24a4ffa7ba69f332fbecd9de
Gautier Kiss <gkgkgk395@gmail.com>
2023-06-01 11:30:03 +0200

    [AM] new interface

--- src/components/pages/aboutMePage/AboutMePage.tsx
+++ src/components/pages/aboutMePage/AboutMePage.tsx
@@ -1,13 +1,36 @@
 import React, { useEffect } from "react"
+import * as THREE from 'three';
 import './AboutMePage.scss'
 import Info from '../../info/Info'
+import ExperienceList from "./components/ExperienceList"
 
 const AboutMePage: React.FC<any> = () => {
     const studies: string[] = ["Software Engineering at 19 coding school", "Management Computing at EPFC Brussels"]
     const jobs: string[] = ["Java developer / Software Engineer at BNP Pariba Fortis"]
     const skills: string[] = ["Java", "C", "C++", "HTML", "CSS", "JavaScript", "TypeScript"]
+    const skills2: string[] = ["Java", "C", "C++", "HTML", "CSS", "JS", "TS"]
+
     
-    
+
+    useEffect(() => {
+        const degrees = 360 / skills2.length;
+        const amSkills = document.getElementById("amSkills");
+        if(amSkills != null) {
+            for(let i = 0; i < skills2.length; ++i) {
+                const node = document.createElement("p");
+                node.textContent = skills2[i];
+                node.className = "test";
+                node.style.top = `calc(375px + (400px * sin(${degrees * i}deg)))`
+                node.style.right = `calc(375px + (400px * cos(${degrees * i}deg)))`
+                node.style.transform = `rotateX(-90deg) rotateY(${degrees * i + -90}deg)`;
+                console.log(amSkills);
+                console.log(amSkills.clientHeight);
+                amSkills?.appendChild(node);
+            }
+        }
+    })
+
+    /*
     return (
         <section className="aboutMePage">
             <div className="aboutMeSection">
@@ -18,6 +41,18 @@ const AboutMePage: React.FC<any> = () => {
                 <Info title="Studies" content={studies}></Info>
                 <Info title="Jobs" content={jobs}></Info>
                 <Info title="Skills" content={skills}></Info>
+            </div>
+        </section>
+    )
+    */
+
+    return (
+        <section className="aboutMePage" id="aboutMePage">
+            <h1 className="amTitle">My Resume</h1>
+            <p className="amDescription">What I like about programming is that there is always something new to learn. For me it is a passion more than a full time job.</p>
+            <ExperienceList></ExperienceList>
+            <div className="amSkills" id="amSkills">
+
             </div>
         </section>
     )
---
+++ src/components/pages/aboutMePage/components/Experience.tsx
@@ -0,0 +1,14 @@
+const Experience: React.FC<any> = () => {
+    return (
+        <div className="amExperience">
+            <p className="amExperienceYear">2022</p>
+            <div className="amExperienceJob">
+                <p className="amExperienceTitle">Java Developer/Software Engineer</p>
+                <p className="amExperiencePlace">BNPPF</p>
+            </div>
+            <p className="amExperienceDescription">Development  of backend api’s. I amin charge of the data structure, DB management and queries using MySql, Oracle and Java8.</p>
+        </div>
+    )
+}
+
+export default Experience;
